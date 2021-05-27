b46b0106afc6494dcf3ceef52af2c7bf6b3c4234
meli9er <magnusoncannady.melissa@gmail.com>
2021-05-27 15:58:09 -0400

    updates before Final Class Presentations

--- eMarker_App_Preview_Page/app.js
+++ eMarker_App_Preview_Page/app.js
@@ -1,28 +1,25 @@
 // console.log(10)
 
-// when curser enters "Get Notified" area, image changes
 
-    let gnbox = document.getElementById("notified")
-    let vibox = document.getElementById("info")
-    let spbox = document.getElementById("photos")
-    let embox = document.getElementById("fromhome")
+let gnbox = document.getElementById("notified")
+let vibox = document.getElementById("info")
+let spbox = document.getElementById("photos")
+let embox = document.getElementById("fromhome")
 
-/////////// Get Notified
+/////////// Get Notified Area
 
-    gnbox.addEventListener('mouseover', function(){
-        let featureimage = document.getElementById("homescreen")
-        // console.log(featureimage)
-        featureimage.src = "images/MapPagewNotification.jpg"
-    })
+gnbox.addEventListener('mouseover', function(){
+    let featureimage = document.getElementById("homescreen")
+    // console.log(featureimage)
+    featureimage.src = "images/MapPagewNotification.jpg"
+})
 
-// when curser leaves area, image returns to original
+gnbox.addEventListener('mouseout', function(){
+    let featureimage = document.getElementById("homescreen")
+    featureimage.src = "images/HomeScreen.jpg"
+})
 
-    gnbox.addEventListener('mouseout', function(){
-        let featureimage = document.getElementById("homescreen")
-        featureimage.src = "images/HomeScreen.jpg"
-    })
-
-///////// View Info
+/////////// View Info Area
 
 vibox.addEventListener('mouseover', function(){
     let featureimage = document.getElementById("homescreen")
@@ -30,82 +27,31 @@ vibox.addEventListener('mouseover', function(){
     featureimage.src = "images/MarkerInfo.jpg"
 })
 
-// when curser leaves area, image returns to original
-
 vibox.addEventListener('mouseout', function(){
     let featureimage = document.getElementById("homescreen")
     featureimage.src = "images/HomeScreen.jpg"
 })
 
-
-/////////////////// SEe Photos
+/////////// See Photos Area
 
 spbox.addEventListener('mouseover', function(){
     let featureimage = document.getElementById("homescreen")
     featureimage.src = "images/MarkerImages.jpg"
 })
 
-// when curser leaves area, image returns to original
-
 spbox.addEventListener('mouseout', function(){
     let featureimage = document.getElementById("homescreen")
     featureimage.src = "images/HomeScreen.jpg"
 })
 
-
-///////////// Explore Markers
+/////////// Explore Markers Area
 
 embox.addEventListener('mouseover', function(){
     let featureimage = document.getElementById("homescreen")
     featureimage.src = "images/InDepthMarkerInfo.jpg"
 })
 
-// when curser leaves area, image returns to original
-
 embox.addEventListener('mouseout', function(){
     let featureimage = document.getElementById("homescreen")
     featureimage.src = "images/HomeScreen.jpg"
 })
-
-
-
-
-
-    // get area by ID
-
-    // identify what image needs to change
-
-    // get image that needs to change
-
-    // set image to new image
-
-
-
-// // hover over area to change a photo
-// var image = document.getElementById("notified");
-// console.log(image)
-
-// notified.addEventListener('mouseover', function(){
-//     image.src = "/images/MapPagewNotification.jpg"
-// })
-
-
-
-
-
-
-
-
-
-
-///////example from stackoverflow
-// var image = document.getElementById("image");
-
-// //Now, we need to add an Event Listener to listen when the image gets mouse over.
-
-// image.addEventListener('mouseover', function(){
-//   image.src = "path/to/newimage"
-// })
-// image.addEventListener('mouseout', function(){
-//     image.src = "path/to/otherimage"
-//   })
